670d70ff29b803911b51356bb9fcdf4f9c19ce8b
Mat Gadd <source@catchall.drarok.com>
2024-01-25 11:48:55 +0000

    Update demo to use new structure and features

--- LoopedVideoPlayerDemo/LoopedVideoPlayerDemo/DemoView.swift
+++ LoopedVideoPlayerDemo/LoopedVideoPlayerDemo/DemoView.swift
@@ -11,6 +11,15 @@ import UIKit
 import LoopedVideoPlayer
 
 class DemoView: BaseView {
+    private let stackView: UIStackView = {
+        let view = UIStackView(frame: .zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.axis = .vertical
+        view.isLayoutMarginsRelativeArrangement = true
+        view.layoutMargins = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
+        return view
+    }()
+
     private(set) lazy var playSingleVideoButton: UIButton = {
         let button = UIButton(type: .system)
         button.translatesAutoresizingMaskIntoConstraints = false
@@ -23,28 +32,48 @@ class DemoView: BaseView {
         return button
     }()
 
+    private(set) lazy var separatorView: UIView = {
+        let view = UIView(frame: .zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    private(set) lazy var playSingleVideoWithCustomUIButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    private(set) lazy var playPlaylistWithCustomUIButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
     override func setupSubviews() {
         let background = UIColor(named: "background")
         self.backgroundColor = background
-        self.addSubview(self.playSingleVideoButton)
-        self.addSubview(self.playPlaylistButton)
+
+        self.separatorView.backgroundColor = .lightGray
+
+        self.stackView.addArrangedSubview(self.playSingleVideoButton)
+        self.stackView.addArrangedSubview(self.playPlaylistButton)
+        self.stackView.addArrangedSubview(self.separatorView)
+        self.stackView.addArrangedSubview(self.playSingleVideoWithCustomUIButton)
+        self.stackView.addArrangedSubview(self.playPlaylistWithCustomUIButton)
+        self.addSubview(self.stackView)
     }
 
     override func setupConstraints() {
         let layout = self.safeAreaLayoutGuide
 
         NSLayoutConstraint.activate([
-            self.playSingleVideoButton.leadingAnchor.constraint(greaterThanOrEqualTo: layout.leadingAnchor),
-            self.playSingleVideoButton.trailingAnchor.constraint(lessThanOrEqualTo: layout.trailingAnchor),
-            self.playSingleVideoButton.topAnchor.constraint(equalToSystemSpacingBelow: layout.topAnchor, multiplier: 1),
-            self.playSingleVideoButton.centerXAnchor.constraint(equalTo: layout.centerXAnchor),
-        ])
+            self.stackView.leadingAnchor.constraint(equalTo: layout.leadingAnchor),
+            self.stackView.trailingAnchor.constraint(equalTo: layout.trailingAnchor),
+            self.stackView.topAnchor.constraint(equalTo: layout.topAnchor),
+            self.stackView.bottomAnchor.constraint(lessThanOrEqualTo: layout.bottomAnchor),
 
-        NSLayoutConstraint.activate([
-            self.playPlaylistButton.leadingAnchor.constraint(greaterThanOrEqualTo: layout.leadingAnchor),
-            self.playPlaylistButton.trailingAnchor.constraint(lessThanOrEqualTo: layout.trailingAnchor),
-            self.playPlaylistButton.topAnchor.constraint(equalToSystemSpacingBelow: self.playSingleVideoButton.bottomAnchor, multiplier: 1),
-            self.playPlaylistButton.centerXAnchor.constraint(equalTo: layout.centerXAnchor),
+            self.separatorView.heightAnchor.constraint(equalToConstant: 1),
         ])
     }
 }
